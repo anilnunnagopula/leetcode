f36f2f19bded6a04bb0cacdfc6f96f474fb7db52
Anil Nunnagopula <btech2k26@gmail.com>
2025-10-14 00:58:33 +0530

    Time: 3 ms (55.46%), Space: 44.6 MB (92.8%) - LeetHub

--- 1353-find-resultant-array-after-removing-anagrams/1353-find-resultant-array-after-removing-anagrams.java
+++ 1353-find-resultant-array-after-removing-anagrams/1353-find-resultant-array-after-removing-anagrams.java
@@ -1,26 +1,5 @@
 class Solution {
     public List<String> removeAnagrams(String[] words) {
-        // LinkedHashSet<String> set=new LinkedHashSet<>();
-        // List l=new ArrayList<>();
-        // for(int i=0;i<words.length;i++){
-        //     StringBuilder sb=new StringBuilder(words[i]);
-        //     // sb.sort();
-        //     char[] ch = words[i].toCharArray();  // convert to char array
-        //     Arrays.sort(ch);                     // sort the characters
-        //     String sorted = new String(ch);      // back to string
-        //     if(set.contains(sorted)){
-
-        //     }
-        //     else{
-        //         l.add(words[i]);
-        //     }
-        //     set.add(sorted);   
-        //     // set.add(sb.toString());
-        // }
-        // // for(String s:set){
-        // //     l.add(s);
-        // // }
-        // return l;
 
         int n=words.length;
         String prev=words[0];
@@ -37,7 +16,6 @@ class Solution {
                 prev=words[i];
             }
         }
-        
         return l;
 
 
