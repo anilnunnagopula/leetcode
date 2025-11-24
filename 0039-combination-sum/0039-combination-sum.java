dd8e2d6083146217454c26b582422ac12fe40393
Anil Nunnagopula <btech2k26@gmail.com>
2025-11-24 22:30:55 +0530

    Time: 2 ms (89.34%), Space: 45.7 MB (23.47%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.java
@@ -0,0 +1,29 @@
+class Solution {
+    public void recursiveHelper(List<List<Integer>> l,ArrayList<Integer> al,int n,int target,int a[],int curIdx){
+        if(curIdx>=n||target<0){
+            return;
+        }
+
+        if(target==0){
+            l.add(new ArrayList<>(al));
+            // al.remove(al.size()-1);
+            return;
+        }
+        al.add(a[curIdx]);
+
+
+        recursiveHelper(l,al,n,target-a[curIdx],a,curIdx);
+        al.remove(al.size()-1);
+        recursiveHelper(l,al,n,target,a,curIdx+1);
+
+    }
+    public List<List<Integer>> combinationSum(int[] a, int target) {
+        List<List<Integer>> l=new ArrayList<>();
+        ArrayList<Integer> al=new ArrayList<>();
+        int n=a.length;
+        // for(int i=0;i<n;i++){
+            recursiveHelper(l,al,n,target,a,0);
+        // }
+        return l;
+    }
+}
